7fcecea958e18d185d24955270becd7dce1af366
Zach Denton <z@chdenton.com>
2016-11-27 00:26:47 -0800

    Add support for remixes.

--- whatapi.py
+++ whatapi.py
@@ -64,6 +64,7 @@ def album_release_type(album):
         "compilation": 7,
         "single": 9,
         "live": 11,
+        "remix": 13,
         "other": 1,
         None: 1
     }[album.albumtype]
